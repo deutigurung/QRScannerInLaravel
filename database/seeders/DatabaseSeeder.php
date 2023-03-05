d8eac724b6ff3552eaee93e90325945f9f247516
Deuti Gurung <gurungdeuti@gmail.com>
2023-03-05 11:32:21 +0545

    readme file chanage

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(5)->create();
-        // \App\Models\Department::factory(2)->create();
+        \App\Models\User::factory(5)->create();
+        \App\Models\Department::factory(2)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
