2874790752376c44278b8c6248d64917b986ad06
Deuti Gurung <gurungdeuti@gmail.com>
2023-03-05 08:00:22 +0545

    blade page update

--- app/Http/Controllers/QrCodeController.php
+++ app/Http/Controllers/QrCodeController.php
@@ -11,7 +11,7 @@ class QrCodeController extends Controller
     public function index($userId)
     {
         $user = User::find($userId);
-        $department = Department::find(7);
+        $department = Department::find(1);
         $check_user = $department->users()->find($user->id);
         return view('qrcode',compact('check_user'));
     }
